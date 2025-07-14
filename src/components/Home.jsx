a9138cf3d14cc6dc5bd6b04442a98b7025a04908
kanwarsourav <souravkanwar007@gmail.com>
2025-07-14 13:33:15 +0530

    old

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ import Enquirys from './Enquirys'
 import Blogs from './Blogs'
 import Water from './Water'
 import Banner from './Banner'
-import FadeInSection from './FadeIn'
+
 
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
@@ -21,19 +21,20 @@ export default function Home() {
   return (
     <>
   
-    
-      <FadeInSection direction="right"><Banner /></FadeInSection>
-      <FadeInSection direction="left"><OurCompany /></FadeInSection>
-      <FadeInSection direction="right"><WorkVisa /></FadeInSection>
-      <FadeInSection direction="left"><HelpClient /></FadeInSection>
-      <FadeInSection direction="right"><OurConsultancy /></FadeInSection>
-      <FadeInSection direction="left"><Flags /></FadeInSection>
-      <FadeInSection direction="right"><Testimonial /></FadeInSection>
-      <FadeInSection direction="left"><WhyChooseUs /></FadeInSection>
-      <FadeInSection direction="right"><Detail /></FadeInSection>
-      <FadeInSection direction="left"><Enquirys /></FadeInSection>
-      <FadeInSection direction="right"><Blogs /></FadeInSection>
-      <FadeInSection direction="left"><Water /></FadeInSection>
+      <Banner />
+      <OurCompany />
+      <WorkVisa />
+      <HelpClient />
+      <OurConsultancy />
+      <Flags />
+      <Testimonial />
+      <WhyChooseUs />
+      <Detail />
+      <Enquirys />
+      <Blogs />
+      <Water />
+
+      
 
   
     </>
